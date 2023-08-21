9e31532dee3a9e54d75888a3a7e440d1aa4181f1
Pedro Mamed <mamed14.pedro@gmail.com.br>
2023-08-21 11:45:00 -0300

    Aula e exercícios sobre laços

---
+++ laco_while.py
@@ -0,0 +1,11 @@
+'''
+Exibir tdos os números exstentes na sêquencia da 1 a 500.
+'''
+
+contador = 1
+
+while (contador <= 500):
+    print(f"contador: {contador}")
+    contador = contador + 1
+
+print("Depois do bloco do while")
